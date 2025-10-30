023b20d96193d4c1f27ba7c9cff17a63a085f5c2
AyubAz <110174399+ayubAz@users.noreply.github.com>
2025-10-30 22:51:37 +0000

    refac AgentAddressPage to use navigator

--- app/controllers/manageAgents/AgentNameController.scala
+++ app/controllers/manageAgents/AgentNameController.scala
@@ -20,14 +20,15 @@ import controllers.actions.*
 import forms.manageAgents.AgentNameFormProvider
 import models.Mode
 import navigation.Navigator
-import pages.manageAgents.{AgentNamePage, AgentAddressPage}
+import pages.manageAgents.{AgentAddressPage, AgentNameDuplicateWarningPage, AgentNamePage}
 import javax.inject.Inject
 import play.api.i18n.{I18nSupport, MessagesApi}
 import play.api.mvc.{Action, AnyContent, MessagesControllerComponents}
 import repositories.SessionRepository
 import services.StampDutyLandTaxService
 import uk.gov.hmrc.play.bootstrap.frontend.controller.FrontendBaseController
-import views.html.manageAgent.AgentNameView
+import views.html.manageAgents.AgentNameView
+import controllers.manageAgents.routes.*
 
 import scala.concurrent.{ExecutionContext, Future}
 
@@ -42,7 +43,7 @@ class AgentNameController@Inject()(
                                     stornRequiredAction: StornRequiredAction,
                                     stampDutyLandTaxService: StampDutyLandTaxService,
                                     view: AgentNameView,
-                                    navigator: Navigator,
+                                    navigator: Navigator
                                   )(implicit ec: ExecutionContext) extends FrontendBaseController with I18nSupport {
 
   val form = formProvider()
@@ -56,27 +57,22 @@ class AgentNameController@Inject()(
 
     Ok(view(preparedForm, mode))
   }
-  
 
-  def onSubmit(mode: Mode): Action[AnyContent] = ( identify andThen getData andThen requireData andThen stornRequiredAction).async { implicit request =>
-
-  form
-    .bindFromRequest()
-    .fold(
-      formWithErrors => Future.successful(BadRequest(view(formWithErrors, mode))),
-      value => {
-        stampDutyLandTaxService
-          .isDuplicate(request.storn: String, value)
-          .flatMap {
-            case true =>
-              Future.successful(Redirect(controllers.manageAgents.routes.WarningAgentNameController.onPageLoad(mode)))
-            case false =>
-              for {
-                updatedAnswers <- Future.fromTry(request.userAnswers.set(AgentNamePage, value))
-                _ <- sessionRepository.set(updatedAnswers)
-              } yield Redirect(navigator.nextPage(AgentAddressPage, mode, updatedAnswers))
+  def onSubmit(mode: Mode): Action[AnyContent] = (identify andThen getData andThen requireData andThen stornRequiredAction).async { implicit request =>
+    form
+      .bindFromRequest()
+      .fold(
+        formWithErrors => Future.successful(BadRequest(view(formWithErrors, mode))),
+        value =>
+          for {
+            updatedAnswers <- Future.fromTry(request.userAnswers.set(AgentNamePage, value))
+            isDuplicate    <- stampDutyLandTaxService.isDuplicate(request.storn: String, value)
+            _              <- sessionRepository.set(updatedAnswers)
+          } yield if (isDuplicate) {
+            Redirect(navigator.nextPage(AgentNameDuplicateWarningPage, mode, updatedAnswers))
+          } else {
+            Redirect(navigator.nextPage(AgentAddressPage, mode, updatedAnswers))
           }
-      }
-    )
-}
+      )
+  }
 }
