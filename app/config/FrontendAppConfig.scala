3bb1a9bac3398060796dff9b917a7d4d5f4a02a9
avelx <3528155+avelx@users.noreply.github.com>
2025-10-31 14:30:55 +0000

    DTR-896: * move files across from next PR: https://github.com/hmrc/sdlt-agent-frontend/pull/8; * reason: original PR was created with not verified commits

--- app/config/FrontendAppConfig.scala
+++ app/config/FrontendAppConfig.scala
@@ -54,4 +54,13 @@ class FrontendAppConfig @Inject() (configuration: Configuration) {
   val countdown: Int = configuration.get[Int]("timeout-dialog.countdown")
 
   val cacheTtl: Long = configuration.get[Int]("mongodb.timeToLiveInSeconds")
+
+  val sessionTimeOut: Long = configuration.get[Long]("session.timeoutSeconds")
+  
+  // AddressLookup configuration
+  private val addressLookupPort: String = configuration.get[String]("address-lookup-frontend.port")
+  private val addressLookupHost: String = configuration.get[String]("address-lookup-frontend.host")
+  private val addressLookupProtocol: String = configuration.get[String]("address-lookup-frontend.protocol")
+  val addressLookupBaseUrl: String = s"$addressLookupProtocol://$addressLookupHost:$addressLookupPort"
+  val addressLookupTimeoutUrl: String = configuration.get[String]("address-lookup-frontend.timeoutUrl")
 }
