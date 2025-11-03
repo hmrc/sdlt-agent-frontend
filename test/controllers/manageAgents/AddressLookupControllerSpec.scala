0fba16778556869eb3bc4dd8dab2cb247bba0456
avelx <3528155+avelx@users.noreply.github.com>
2025-11-03 16:07:01 +0000

    DTR-896: *  improve test coverage

--- test/controllers/manageAgents/AddressLookupControllerSpec.scala
+++ test/controllers/manageAgents/AddressLookupControllerSpec.scala
@@ -33,20 +33,21 @@
 package controllers.manageAgents
 
 import base.SpecBase
-import models.{NormalMode, UserAnswers}
 import models.responses.addresslookup.JourneyInitResponse.{JourneyInitFailureResponse, JourneyInitSuccessResponse}
-import org.mockito.ArgumentMatchers.any
-import org.mockito.Mockito.when
+import models.responses.addresslookup.JourneyOutcomeResponse.UnexpectedGetStatusFailure
+import models.{CheckMode, Mode, NormalMode, UserAnswers}
+import org.mockito.ArgumentMatchers.{any, eq as eqTo}
+import org.mockito.Mockito.{times, verify, when}
 import org.scalatestplus.mockito.MockitoSugar
 import play.api.Application
 import play.api.i18n.Messages
-import services.AddressLookupService
 import play.api.inject.bind
 import play.api.mvc.AnyContentAsEmpty
 import play.api.test.FakeRequest
+import play.api.test.Helpers.*
+import services.AddressLookupService
 import uk.gov.hmrc.http.HeaderCarrier
 import uk.gov.hmrc.http.HttpVerbs.GET
-import play.api.test.Helpers.*
 
 import scala.concurrent.Future
 
@@ -62,7 +63,7 @@ class AddressLookupControllerSpec extends SpecBase with MockitoSugar {
     val addressLookupInit: String = controllers.manageAgents.routes.AddressLookupController.onPageLoad(NormalMode).url
 
     val id: String = "idToExtractAddress"
-    val addressLookupExtract: String = controllers.manageAgents.routes.AddressLookupController.onSubmit(NormalMode).url + s"?id=$id"
+    val addressLookupExtract: Mode => String = (mode: Mode) => controllers.manageAgents.routes.AddressLookupController.onSubmit(mode).url + s"?id=$id"
     val thereIsProblemUrl : String = controllers.routes.JourneyRecoveryController.onPageLoad(None).url()
 
     val userId: String = "userID"
@@ -121,33 +122,36 @@ class AddressLookupControllerSpec extends SpecBase with MockitoSugar {
   "AddressLookupController get AL journey result" - {
 
     "return AddressDetails on success" in new Fixture {
-// TODO: test disabled while integration for prev/next screen in progress
-//      when(service.getJourneyOutcome(any(), any())(any[HeaderCarrier]))
-//        .thenReturn(Future.successful(Right(userAnswers)))
-//
-//      running(app) {
-//        val request =
-//          FakeRequest(GET, addressLookupExtract)
-//
-//        val result = route(app, request).value
-//
-//        status(result) mustEqual SEE_OTHER
-//        redirectLocation(result).value mustEqual "/stamp-duty-land-tax-agent"
-//      }
-    }
-
-    "return AddressDetails on error" in new Fixture {
       when(service.getJourneyOutcome(any(), any())(any[HeaderCarrier]))
         .thenReturn(Future.successful(Right(userAnswers)))
 
       running(app) {
         val request =
-          FakeRequest(POST, addressLookupExtract)
+          FakeRequest(GET, addressLookupExtract(NormalMode))
 
-        // TODO: we should have redirect to the default Journey Error Page
-        route(app, request).value
+        val result = route(app, request).value
+
+        status(result) mustEqual SEE_OTHER
+        redirectLocation(result).value mustEqual "/stamp-duty-land-tax-agent/manage-agents/agent-contact-details"
+
+        verify(service, times(1)).getJourneyOutcome(any(), any())(any[HeaderCarrier])
       }
+    }
 
+    "return failure on error" in new Fixture {
+      when(service.getJourneyOutcome(any(), any())(any[HeaderCarrier]))
+        .thenReturn(Future.successful(Left(new Error("Some Error")) ))
+
+      running(app) {
+        val request =
+          FakeRequest(GET, addressLookupExtract(NormalMode) )
+
+        val result = route(app, request).value
+
+        status(result) mustEqual SEE_OTHER
+        redirectLocation(result).value mustEqual "/stamp-duty-land-tax-agent/there-is-a-problem"
+        verify(service, times(1)).getJourneyOutcome(any(), any())(any[HeaderCarrier])
+      }
     }
   }
 }
