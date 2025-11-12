85dac06c608d0a5de40f2888240ea6c488b34738
AyubAz <110174399+ayubAz@users.noreply.github.com>
2025-11-12 22:39:35 +0000

    remove houseNumber - it is part of addressLine1

--- app/utils/PaginationHelper.scala
+++ app/utils/PaginationHelper.scala
@@ -65,7 +65,7 @@ trait PaginationHelper {
             )
               .withCssClass("govuk-!-width-one-third govuk-!-font-weight-regular hmrc-summary-list__key"),
             value = ValueViewModel(
-              Text(agentDetails.getFirstLineOfAddress)
+              Text(agentDetails.addressLine1)
             )
               .withCssClass("govuk-summary-list__value govuk-!-width-one-third"),
             actions = Seq(
