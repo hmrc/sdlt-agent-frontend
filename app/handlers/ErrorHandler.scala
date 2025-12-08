8a6d401424e4e6de464ec5285d35bf2440c8c562
Roshangautam2052 <106965552+Roshangautam2052@users.noreply.github.com>
2025-12-08 12:56:14 +0000

    DTR-1715:Implement overriding in ErrorHandler
    Signed-off-by: Roshangautam2052 <106965552+Roshangautam2052@users.noreply.github.com>

--- app/handlers/ErrorHandler.scala
+++ app/handlers/ErrorHandler.scala
@@ -17,25 +17,26 @@
 package handlers
 
 import config.FrontendAppConfig
+
 import javax.inject.{Inject, Singleton}
 import play.api.i18n.{I18nSupport, MessagesApi}
 import play.api.mvc.RequestHeader
 import play.twirl.api.Html
 import uk.gov.hmrc.play.bootstrap.frontend.http.FrontendErrorHandler
-import views.html.{AccessDeniedView, ErrorTemplate, PageNotFoundView}
+import views.html.{AccessDeniedView, ErrorTemplate, PageNotFoundView, SystemErrorView}
 
 import scala.concurrent.{ExecutionContext, Future}
 
 @Singleton
 class ErrorHandler @Inject()(
                               val messagesApi: MessagesApi,
-                              view: ErrorTemplate,
+                              systemErrorView: SystemErrorView,
                               accessDeniedView: AccessDeniedView,
                               notFoundView: PageNotFoundView
                             )(implicit val ec: ExecutionContext, appConfig: FrontendAppConfig) extends FrontendErrorHandler with I18nSupport {
 
   override def standardErrorTemplate(pageTitle: String, heading: String, message: String)(implicit rh: RequestHeader): Future[Html] =
-    Future.successful(view(pageTitle, heading, message))
+    Future.successful(systemErrorView())
 
   override def fallbackClientErrorTemplate(implicit request: RequestHeader): Future[Html] =
     Future.successful(accessDeniedView())
