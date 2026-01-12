3b0b4b7d8ef8f5683247574e3aa758ecef494265
avelx <3528155+avelx@users.noreply.github.com>
2026-01-12 14:34:36 +0000

    DTR-2393::refactor: align agent-FE file structure with manage-FE file structure;

---
+++ app/controllers/actions/AuthenticatedIdentifierAction.scala
@@ -0,0 +1,95 @@
+/*
+ * Copyright 2026 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package controllers.actions
+
+import config.FrontendAppConfig
+import controllers.routes
+import models.requests.IdentifierRequest
+import play.api.Logging
+import play.api.mvc.{BodyParsers, Request, Result}
+import play.api.mvc.Results.Redirect
+import uk.gov.hmrc.auth.core.AffinityGroup.{Individual, Organisation}
+import uk.gov.hmrc.auth.core.AuthProvider.GovernmentGateway
+import uk.gov.hmrc.auth.core.{AuthConnector, AuthProviders, AuthorisationException, AuthorisedFunctions, Enrolment, Enrolments, NoActiveSession, User}
+import uk.gov.hmrc.auth.core.authorise.Predicate
+import uk.gov.hmrc.auth.core.retrieve.v2.Retrievals
+import uk.gov.hmrc.auth.core.retrieve.~
+import uk.gov.hmrc.http.HeaderCarrier
+import uk.gov.hmrc.play.http.HeaderCarrierConverter
+
+import javax.inject.Inject
+import scala.concurrent.{ExecutionContext, Future}
+
+class AuthenticatedIdentifierAction @Inject()(
+                                               override val authConnector: AuthConnector,
+                                               config: FrontendAppConfig,
+                                               val parser: BodyParsers.Default
+                                             )
+                                             (implicit val executionContext: ExecutionContext) extends IdentifierAction with AuthorisedFunctions with Logging {
+
+  override def invokeBlock[A](request: Request[A], block: IdentifierRequest[A] => Future[Result]): Future[Result] = {
+
+    implicit val hc: HeaderCarrier = HeaderCarrierConverter.fromRequestAndSession(request, request.session)
+
+    val defaultPredicate: Predicate = AuthProviders(GovernmentGateway)
+
+    authorised(defaultPredicate)
+      .retrieve(
+        Retrievals.internalId and
+          Retrievals.allEnrolments and
+          Retrievals.affinityGroup and
+          Retrievals.credentialRole
+      ) {
+        //TODO: Add more cases to log and handle error response for missing items eg missing Organisation
+        case Some(internalId) ~ Enrolments(enrolments) ~ Some(Organisation) ~ Some(User) =>
+          hasSdltOrgEnrolment(enrolments)
+            .map { storn =>
+              block(IdentifierRequest(request, internalId, storn))
+            }
+            .getOrElse(
+              Future.successful(
+                Redirect(routes.AccessDeniedController.onPageLoad()))
+            )
+
+        case Some(_) ~ _ ~ Some(Individual) ~ _ =>
+          logger.info("AuthenticatedIdentifierAction - Individual login attempt")
+          Future.successful(
+            Redirect(controllers.routes.UnauthorisedIndividualAffinityController.onPageLoad())
+          )
+      } recover {
+      case _: NoActiveSession =>
+        Redirect(config.loginUrl, Map("continue" -> Seq(config.loginContinueUrl)))
+      case _: AuthorisationException =>
+        Redirect(routes.UnauthorisedController.onPageLoad())
+    }
+  }
+
+  private def hasSdltOrgEnrolment[A](enrolments: Set[Enrolment]): Option[String] =
+    enrolments.find(_.key == "IR-SDLT-ORG") match {
+      case Some(enrolment) =>
+        val storn = enrolment.identifiers.find(id => id.key == "STORN").map(_.value)
+        val isActivated = enrolment.isActivated
+        (storn, isActivated) match {
+          case (Some(storn), true) =>
+            Some(storn)
+          case _ =>
+            logger.error("EnrolmentAuthIdentifierAction - Unable to retrieve sdlt enrolments")
+            None
+        }
+      case _ => None
+    }
+}
