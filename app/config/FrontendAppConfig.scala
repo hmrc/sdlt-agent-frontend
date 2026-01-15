cb1e6d4424443b0fab52c80fb4091089fd1fa8a5
DavouJ <96893264+DavouJ@users.noreply.github.com>
2026-01-15 15:27:50 +0000

    DTR-2108: Sign-out links

--- app/config/FrontendAppConfig.scala
+++ app/config/FrontendAppConfig.scala
@@ -36,10 +36,6 @@ class FrontendAppConfig @Inject() (configuration: Configuration) {
   def feedbackUrl(implicit request: RequestHeader): String =
     s"$contactHost/contact/beta-feedback?service=$contactFormServiceIdentifier&backUrl=${host + request.uri}"
 
-  private val contactUrl: String = configuration.get[Service]("contact-frontend").baseUrl
-  def contactUrl(implicit request: RequestHeader): String =
-    s"$contactUrl/contact/beta-feedback?service=$contactFormServiceIdentifier&backUrl=${host + request.uri}"
-
   private val basGatewayBaseUrl: String = configuration.get[Service]("microservice.services.bas-gateway").baseUrl
   val signOutUrl: String                = s"$basGatewayBaseUrl/bas-gateway/sign-out-without-state"
 
