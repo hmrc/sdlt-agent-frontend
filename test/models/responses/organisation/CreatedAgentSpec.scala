1dbc5b62c9bbe5fc46bc1bdf6dc448256ec01b99
Roshan Gautam <106965552+Roshangautam2052@users.noreply.github.com>
2026-01-29 13:55:36 +0000

    DTR-2875:Bug Fix:Made addressLine1AndAddressLine2 to appear correctly at AgentOverview Page (#79)
    * DTR-2875:Bug Fix:Made addressLine1AndAddressLine2 to appear correctly at AgentOverview Page
    
    Signed-off-by: Roshangautam2052 <106965552+Roshangautam2052@users.noreply.github.com>
    
    * DTR-2875:Add unit test for CreatedAgent
    
    Signed-off-by: Roshangautam2052 <106965552+Roshangautam2052@users.noreply.github.com>
    
    ---------
    
    Signed-off-by: Roshangautam2052 <106965552+Roshangautam2052@users.noreply.github.com>

---
+++ test/models/responses/organisation/CreatedAgentSpec.scala
@@ -0,0 +1,66 @@
+/*
+ * Copyright 2026 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package models.responses.organisation
+
+import org.scalatest.matchers.must.Matchers
+import org.scalatest.wordspec.AnyWordSpec
+
+class CreatedAgentSpec extends AnyWordSpec with Matchers {
+
+  def generateCreatedAgent(inputAddress2: Option[String]): CreatedAgent = CreatedAgent(
+    storn = "STNOO1",
+    agentId = Some("123"),
+    name = "John Doe",
+    houseNumber = None,
+    address1 = "4",
+    address2 = inputAddress2,
+    address3 = None,
+    address4 = None,
+    postcode = None,
+    phone = Some("0234567898"),
+    email = Some("test@email.com"),
+    dxAddress = None,
+    agentResourceReference = "56745"
+  )
+
+  "getAddressLine1AndAddressLine2 in CreatedAgent " should {
+
+    "concatenate and return address1 and address2" when {
+
+      "address2 is defined" in {
+
+        val address2 = "Winchester Road"
+
+        val createdAgentWithAddressLine2 = generateCreatedAgent(Some(address2))
+
+        val combinedAddressLine1AndAddressLine2 = s"${createdAgentWithAddressLine2.address1} ${address2}"
+
+        createdAgentWithAddressLine2.getAddressLine1AndAddressLine2 mustBe combinedAddressLine1AndAddressLine2
+      }
+
+      "address2 is not defined" in {
+
+        val createdAgentWithoutAddressLine2 = generateCreatedAgent(None)
+
+        val combinedAddressLine1AndAddressLine2 = s"${createdAgentWithoutAddressLine2.address1}"
+
+        createdAgentWithoutAddressLine2.getAddressLine1AndAddressLine2 mustBe combinedAddressLine1AndAddressLine2
+      }
+    }
+  }
+
+}
